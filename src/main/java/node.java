bc638921ac7495e12290943a2c33e9c36059150d
Daniel Jędrusik <danjed@student.agh.edu.pl>
2015-06-24 00:42:07 +0200

    CRMRND-6969 node

---
+++ src/main/java/node.java
@@ -0,0 +1,23 @@
+/**
+ * Created by daniel on 23.06.15.
+ */
+public class node {
+    public node child = null;
+    public String val = null;
+    public String id = null;
+    public node next = null;
+
+    //Konstruktor dla cup'a
+    public node(node child, node next, String val, String id){
+        this.child = child;
+        this. next = next;
+        this.id = id;
+        this.val = val;
+    }
+
+    //Konstrukor dla jflex'a
+    public node(String val, String id){
+        this.val = val;
+        this.id = id;
+    }
+}
